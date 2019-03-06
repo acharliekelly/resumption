c6faea2b2142d1e6e6bd6c172e06185700c97418
Charlie <charliekelly31415@gmail.com>
2019-03-05 21:35:09 -0500

    Deleted non-Rails files
    Started creating migration files w/o knowing anything about Rails.
    Deleted.

--- db/migrate/012_create_tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE resumes (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  ref_title TEXT,
-  contact_id INTEGER REFERENCES contact_info(id),
-  objective_id INTEGER REFERENCES objectives(id),
-  created_on TIMESTAMP,
-  last_modified TIMESTAMP
-);
