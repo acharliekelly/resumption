8dfd219a377e40dc4c0d56b93d0bb6c14e8eebdf
Charlie <charliekelly31415@gmail.com>
2019-03-09 14:23:40 -0500

    Add format column to sections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_184533) do
+ActiveRecord::Schema.define(version: 2019_03_09_192102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_03_09_184533) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "sectype_id"
+    t.string "format"
     t.index ["sectype_id"], name: "index_sections_on_sectype_id"
     t.index ["user_id"], name: "index_sections_on_user_id"
   end
